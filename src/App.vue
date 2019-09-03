2675c3db98c80c9d6a05042215c183a2e91d0f91
jviriato <josevviriato@gmail.com>
2019-09-02 22:46:50 -0300

    Adiciona menu lateral

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,61 @@
 <template>
   <div id="app">
-    <meta content="width=device-width, initial-scale=1" name="viewport" />
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div> -->
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
+      integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
+      crossorigin="anonymous"
+    />
+    <sidebar-menu  v-if="this.$route.path !== '/'" :menu="menu" collapsed="true" />
     <router-view />
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      menu: [
+        {
+          header: true,
+          title: "Menu",
+          hiddenOnCollapse: true
+        },
+        {
+          href: "/dashboard",
+          title: "Dashboard",
+          icon: "fa fa-user"
+        },
+        {
+          href: "/status",
+          title: "Status",
+          icon: "fa fa-chart-area"
+        },
+        {
+          href: "/enviar-formulario",
+          title: "Enviar ACG",
+          icon: "fa fa-folder"
+        }
+      ]
+    };
+  },
+
+  mounted() {
+    this.$emit('toggle-collapse');
+  }
+};
+</script>
+
 <style lang="scss">
+.v-sidebar-menu .vsm--title {
+  padding-left: 0.5em;
+  text-align: left;
+  color: white;
+  text-decoration: none;
+}
+a:hover {
+  text-decoration: none;
+}
 #app {
   font-family: "Roboto", "Avenir", Helvetica, Arial, sans-serif;
   background: $background-gray;
--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import VeeValidate from "vee-validate";
 import VueProgress from "vue-progress";
+import VueSidebarMenu from 'vue-sidebar-menu';
 import { MdButton, MdContent, MdField, MdCard, MdMenu, MdList, MdDatepicker } from "vue-material/dist/components";
 import "vue-material/dist/vue-material.min.css";
 import "vue-material/dist/theme/default.css";
+import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
 import "./registerServiceWorker";
 Vue.use(VueProgress);
 Vue.use(VeeValidate);
@@ -16,7 +18,7 @@ Vue.use(MdCard);
 Vue.use(MdList);
 Vue.use(MdMenu);
 Vue.use(MdDatepicker);
-
+Vue.use(VueSidebarMenu);
 Vue.config.productionTip = false;
 
 new Vue({
