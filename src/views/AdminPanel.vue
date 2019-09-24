a5b7d76414c95f0824a92e91e53c22fbc4b44956
José Victor Viriato <josevviriato@gmail.com>
2019-09-23 22:56:36 -0300

    wip: stas

--- src/views/AdminPanel.vue
+++ src/views/AdminPanel.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="wrapper">
     <div class="hero">Painel Administrativo</div>
+    <div class="stats">
+      <div class="sub" v-for="(item, key, index) in stats" :key="index">
+        <h3>
+        {{key}}:  
+        </h3>
+        <h3>
+         {{item}}h
+        </h3>
+      </div>
+    </div>
     <div class="table-wrapper">
         <table style="width:100%">
         <tr>
@@ -29,7 +39,19 @@ export default {
   name: "adminPanel",
   data() {
     return {
-      data: []
+      data: [],
+      stats: {
+        'Total': 0,
+        'Atividades de extensão': 0,
+        'Monitoria': 0,
+        'Outras atividades': 0,
+        'Estágios extra-curriculares': 0,
+        'Atuação em núcleo temático': 0,
+        'Atividades de iniciação científica': 0,
+        'Participação em órgãos colegiados e diretórios acadêmicos': 0,
+        'Publicação de trabalhos': 0,
+        'Participação em eventos': 0,
+      }
     };
   },
 
@@ -52,14 +74,42 @@ export default {
           horas_requisitadas: e.horas_requisitadas,
           horas_efetivadas: e.horas_efetivadas
         };
+        this.stats[e.categoria.tipo] += e.horas_efetivadas;
+        this.stats['Total'] += e.horas_efetivadas;
+        // console.log(this.stats)
         this.data.push(aluno);
       });
-    }
+          // this.calcData();
+
+    },
+
+    // calcData() {
+    //   if(this.data) {
+    //     this.data.forEach(d => {
+    //       this.stats[d.categoria] += d.horas_efetivadas;
+    //       this.stats.total += d.horas_efetivadas;
+
+    //       console.log(d.categoria, this.stats[d.categoria] += d.horas_efetivadas)
+    //     })
+    //   }
+    // }
   }
 };
 </script>
 
 <style lang="scss" scoped>
+.stats {
+  height: 30vh;
+  width: 80vw;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  .sub {
+    margin: 1em;
+    display: flex;
+    flex-direction: column;
+  }
+}
 .wrapper {
     display: flex;
     flex-direction: column;
